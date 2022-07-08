3fb2fa85bd661f5d78071d2a2c17ea8bb66367ae
Melikazam <sabamelikishvili@gmail.com>
2022-07-08 21:29:47 +0400

    test

--- University_Project.Mvc/Models/Product.cs
+++ University_Project.Mvc/Models/Product.cs
@@ -9,7 +9,7 @@ namespace University_Project.Mvc.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string? Description { get; set; }
-        public string? Image { get; set; }
+        public string Image { get; set; }
         public int? Category_Id { get; set; }
     }
 }
